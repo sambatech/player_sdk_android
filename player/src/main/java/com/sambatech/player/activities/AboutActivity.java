3d53ba02f3c0b45d276bda42d8ef12d423687b41
Thiago <thiago.miranda@sambatech.com.br>
2016-01-18 16:26:27 -0200

    #20 - acrescentou a view sobre, feedback de carregamento e fix no erro ao voltar no ad

---
+++ player/src/main/java/com/sambatech/player/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.sambatech.player.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.sambatech.player.R;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+
+}
