eee716ac97b8064ac235282fa5b769a7e0dc8f81
kesleyvaz <kesley.vaz@sambatech.com.br>
2018-08-20 17:07:55 -0300

    chromecast fixes 2

---
+++ sambaplayersdk/src/main/java/com/sambatech/player/utils/SharedPrefsUtils.java
@@ -0,0 +1,44 @@
+package com.sambatech.player.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+final public class SharedPrefsUtils {
+
+    public class SharedPrefsKeys {
+        public static final String MEDIA_CASTING_KEY = "MEDIA_CASTING_KEY";
+    }
+
+    private SharedPrefsUtils() {}
+
+    public static String getStringPreference(Context context, String key) {
+        String value = null;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences != null) {
+            value = preferences.getString(key, null);
+        }
+        return value;
+    }
+
+    public static boolean setStringPreference(Context context, String key, String value) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences != null && !TextUtils.isEmpty(key)) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(key, value);
+            return editor.commit();
+        }
+        return false;
+    }
+
+
+    public static void clearPreferenceByKey(Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences != null) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.remove(key);
+            editor.commit();
+        }
+    }
+}
