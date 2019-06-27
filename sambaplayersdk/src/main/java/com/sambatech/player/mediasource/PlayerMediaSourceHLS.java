42d5db7689d03bc65193fc2e29e02f90f44bb858
viniciuslana <contato@viniciuslana.com>
2019-06-27 11:29:35 -0300

    Implements Peer5 integration

--- sambaplayersdk/src/main/java/com/sambatech/player/mediasource/PlayerMediaSourceHLS.java
+++ sambaplayersdk/src/main/java/com/sambatech/player/mediasource/PlayerMediaSourceHLS.java
@@ -6,15 +6,13 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.source.hls.playlist.DefaultHlsPlaylistParserFactory;
 import com.sambatech.player.offline.SambaDownloadManager;
-
-/**
- * Created by luizbyrro on 28/11/2017.
- */
+import com.peer5.sdk.Peer5Sdk;
 
 public class PlayerMediaSourceHLS extends PlayerMediaSource implements PlayerMediaSourceInterface {
 
-    public PlayerMediaSourceHLS(PlayerInstanceDefault playerInstanceDefault, String url) {
+    public PlayerMediaSourceHLS(PlayerInstanceDefault playerInstanceDefault, String url, Boolean enablePeer5) {
         super(playerInstanceDefault);
+        this.setEnablePeer5(enablePeer5);
         this.setUrl(url);
     }
 
@@ -24,6 +22,12 @@ public class PlayerMediaSourceHLS extends PlayerMediaSource implements PlayerMed
         Uri uri = Uri.parse(url);
 
         MediaSource mediaSource;
+        String peer5Url = Peer5Sdk.getPeer5StreamUrl(url);
+        Uri peer5Uri = Uri.parse(peer5Url);
+
+        if(this.getEnablePeer5()) {
+            uri = peer5Uri;
+        }
 
         if (SambaDownloadManager.getInstance().isConfigured()) {
             mediaSource = new HlsMediaSource.Factory(SambaDownloadManager.getInstance().buildDataSourceFactory())
